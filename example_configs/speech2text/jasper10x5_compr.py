acc897813e4a662e2404eb40a3e460b20b9d2853
borisgin <boris.ginsburg@gmail.com>
2019-04-10 18:40:01 -0700

    changed compression=8
    Signed-off-by: borisgin <boris.ginsburg@gmail.com>

--- example_configs/speech2text/jasper10x5_compr.py
+++ example_configs/speech2text/jasper10x5_compr.py
@@ -60,89 +60,89 @@ base_params = {
                 "type": "conv1d", "repeat": 1,
                 "kernel_size": [11], "stride": [2],
                 "num_channels": 256, "padding": "SAME",
-                "dilation":[1], "compression": 1, "dropout_keep_prob" : 0.8,
+                "dilation":[1], "compression": 8, "dropout_keep_prob" : 0.8,
             },
             {
                 "type": "conv1d", "repeat": 5,
                 "kernel_size": [11], "stride": [1],
                 "num_channels": 256, "padding": "SAME",
-                "dilation":[1], "compression": 1, "dropout_keep_prob" : 0.8,
+                "dilation":[1], "compression": 8, "dropout_keep_prob" : 0.8,
                 "residual": True, "residual_dense": residual_dense
             },
             {
                 "type": "conv1d", "repeat": 5,
                 "kernel_size": [11], "stride": [1],
                 "num_channels": 256, "padding": "SAME",
-                "dilation":[1], "compression": 1, "dropout_keep_prob" : 0.8,
+                "dilation":[1], "compression": 8, "dropout_keep_prob" : 0.8,
                 "residual": True, "residual_dense": residual_dense
             },
             {
                 "type": "conv1d", "repeat": 5,
                 "kernel_size": [13], "stride": [1],
                 "num_channels": 384, "padding": "SAME",
-                "dilation":[1], "compression": 1, "dropout_keep_prob" : 0.8,
+                "dilation":[1], "compression": 8, "dropout_keep_prob" : 0.8,
                 "residual": True, "residual_dense": residual_dense
             },
             {
                 "type": "conv1d", "repeat": 5,
                 "kernel_size": [13], "stride": [1],
                 "num_channels": 384, "padding": "SAME",
-                "dilation":[1], "compression": 1, "dropout_keep_prob" : 0.8,
+                "dilation":[1], "compression": 8, "dropout_keep_prob" : 0.8,
                 "residual": True, "residual_dense": residual_dense
             },
             {
                 "type": "conv1d", "repeat": 5,
                 "kernel_size": [17], "stride": [1],
                 "num_channels": 512, "padding": "SAME",
-                "dilation":[1], "compression": 1, "dropout_keep_prob" : 0.8,
+                "dilation":[1], "compression": 8, "dropout_keep_prob" : 0.8,
                 "residual": True, "residual_dense": residual_dense
             },
             {
                 "type": "conv1d", "repeat": 5,
                 "kernel_size": [17], "stride": [1],
                 "num_channels": 512, "padding": "SAME",
-                "dilation":[1], "compression": 1, "dropout_keep_prob" : 0.8,
+                "dilation":[1], "compression": 8, "dropout_keep_prob" : 0.8,
                 "residual": True, "residual_dense": residual_dense
             },
             {
                 "type": "conv1d", "repeat": 5,
                 "kernel_size": [21], "stride": [1],
                 "num_channels": 640, "padding": "SAME",
-                "dilation":[1], "compression": 1, "dropout_keep_prob" : 0.7,
+                "dilation":[1], "compression": 8, "dropout_keep_prob" : 0.7,
                 "residual": True, "residual_dense": residual_dense
             },
             {
                 "type": "conv1d", "repeat": 5,
                 "kernel_size": [21], "stride": [1],
                 "num_channels": 640, "padding": "SAME",
-                "dilation":[1], "compression": 1, "dropout_keep_prob" : 0.7,
+                "dilation":[1], "compression": 8, "dropout_keep_prob" : 0.7,
                 "residual": True, "residual_dense": residual_dense
             },
             {
                 "type": "conv1d", "repeat": 5,
                 "kernel_size": [25], "stride": [1],
                 "num_channels": 768, "padding": "SAME",
-                "dilation":[1], "compression": 1, "dropout_keep_prob" : 0.7,
+                "dilation":[1], "compression": 8, "dropout_keep_prob" : 0.7,
                 "residual": True, "residual_dense": residual_dense
             },
             {
                 "type": "conv1d", "repeat": 5,
                 "kernel_size": [25], "stride": [1],
                 "num_channels": 768, "padding": "SAME",
-                "dilation":[1], "compression": 1, "dropout_keep_prob" : 0.7,
+                "dilation":[1], "compression": 8, "dropout_keep_prob" : 0.7,
                 "residual": True, "residual_dense": residual_dense
             },
             {
                 "type": "conv1d", "repeat": 1,
                 "kernel_size": [29], "stride": [1],
                 "num_channels": 896, "padding": "SAME",
-                "dilation":[2], "compression": 1, "dropout_keep_prob" : 0.6,
+                "dilation":[2], "compression": 8, "dropout_keep_prob" : 0.6,
             },
             {
                 "type": "conv1d", "repeat": 1,
                 "kernel_size": [1], "stride": [1],
                 "num_channels": 1024, "padding": "SAME",
-                "dilation":[1], "compression": 1, "dropout_keep_prob" : 0.6,
+                "dilation":[1], "compression": 8, "dropout_keep_prob" : 0.6,
             }
         ],
 
