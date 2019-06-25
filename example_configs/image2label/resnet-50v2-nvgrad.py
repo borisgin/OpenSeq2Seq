f8a1975fec07359960ecc0d726cc2f91d48bb246
borisgin <boris.ginsburg@gmail.com>
2019-06-25 14:37:44 -0700

    rn50 novograd cosine lr
    Signed-off-by: borisgin <boris.ginsburg@gmail.com>

--- example_configs/image2label/resnet-50v2-nvgrad.py
+++ example_configs/image2label/resnet-50v2-nvgrad.py
@@ -39,16 +39,16 @@ base_params = {
     "grad_averaging": False
   },
 
-  "lr_policy": poly_decay,
-  "lr_policy_params": {
-    "learning_rate": 0.03,
-    "power": 2,
-    "warmup_steps": 0,
-  },
-  # "lr_policy": tf.train.cosine_decay,
+  # "lr_policy": poly_decay,
   # "lr_policy_params": {
   #   "learning_rate": 0.03,
+  #   "power": 2,
+  #   "warmup_steps": 0,
   # },
+  "lr_policy": tf.train.cosine_decay,
+  "lr_policy_params": {
+    "learning_rate": 0.03,
+  },
 
   "initializer": tf.variance_scaling_initializer,
 
